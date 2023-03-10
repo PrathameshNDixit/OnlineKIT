19ca55f83498fcd88dbbb4769409006d57b0cdd5
Prathamesh Dixit <prathameshdixit1234@gmail.com>
2023-03-10 14:24:12 +0530

    og project

---
+++ application/controllers/Branch.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Branch extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+    
+        
+        $this->load->model('Model_Branch');
+        
+    }
+	
+	
+	public function index()
+	{
+        // $data['data']=$this->Branch_model->getallBranch();
+        // echo "<pre>";
+        // print_r($data);
+        $data['data']=$this->Model_Branch->select_all();
+		$this->load->view('common/header_view');
+		$this->load->view('Branch/BranchDetailsview',$data);
+		$this->load->view('common/footer_view');
+	
+
+	}
+    public function create()
+	{
+        $obj='';
+        $branch_id =$this->uri->segment(2);
+
+        if(empty($branch_id)){
+			$branch_id = 0;
+		}
+		
+		if($branch_id > 0){
+			$obj = $this->Model_Branch->search($branch_id);
+		}
+        $data['data']=$obj;
+		$this->load->view('common/header_view');
+		$this->load->view('Branch/Branch_view',$data);
+		$this->load->view('common/footer_view');
+// print_r($data);
+	}
+   
+
+    function insertBranch(){
+    	$branch_id=$this->input->post('branch_id');
+     $branch_name= $this->input->post('branch_name'); 
+   
+
+      
+      
+     
+       $fields=array('branch_name'=>$branch_name,
+                      
+                     
+                      
+             'created_date'=>date('Y-m-d H:i:s'),
+             'created_by'=>1);
+         echo json_encode($fields);
+     $this->Commonmodel->insertRecord("branch_master",$fields);
+   } 
+	
+	
+}
---
+++ web_resources/dist/js/controllers/employee_create.js
@@ -0,0 +1,113 @@
+var a=false;
+$(document).ready(function(){
+$("#btn_save").click(function(){
+  if(a==false){
+    
+    saveperform();
+   }
+  }); 
+});
+
+
+ function saveperform() 
+{ 
+    var fullname=$('#fullname').val();
+    var bld=$('#bld').val();
+    var email=$('#email').val();
+    var dob=$('#dob').val();
+    var emp_gender=$('#emp_gender').val();
+    var age=$('#age').val();
+    var emptype=$('#emptype').val();
+    var Cast=$('#Cast').val();
+    var sCast=$('#sCast').val();
+    var emp_qualification=$('#emp_qualification').val();
+    var department=$('#department').val();
+    var branch=$('#branch').val();
+    var pincode=$('#pincode').val();
+    var adharnumber=$('#adharnumber').val();
+    var phone=$('#phone').val();
+    var country=$('#country').val();
+    var state=$('#state').val();
+    var city=$('#city').val();
+    var address=$('#address').val();
+
+
+    
+
+
+    if(fullname=="") 
+    {
+      // alert("requird");
+        swal({
+        title:"",
+        text:"Please Enter Required Fields",
+        type:"error",           
+    });   
+  }
+
+    // else
+    // {
+    // 	if(userId>0)
+    	// {
+      //       a=true;
+    	
+    		// $.ajax({
+      //   url:base_path+"Country/updateCountry",
+      //   type: "POST",
+      //   data: $('#Form').serialize(),
+      //    beforeSend: function(){
+      //          $('#btn_save').prop('disabled', true);
+      //          $('#btn_save').html('Loading');
+      //     }, 
+      //   success: function(data) {
+      //      $('#btn_save').prop('disabled', false);
+      //      $('#btn_save').html('<img src="'+base_path+'assets/images/save.png" width="21"> Save');
+             
+      //       swal({
+      //       title:"",
+      //       text:"Data Submitted Successfully",
+      //       type:"success",
+      //       showCancelButton: true, 
+      //       showConfirmButton: false,
+      //       timer:10000
+      //   }); a=false;
+      //       window.location.href = base_path+"Country";
+      //     }
+      // });
+    	// }
+        else
+    	{a=true;
+    		//alert('insert');
+    		$.ajax({
+        url:base_path+"Employee/insertEmployee",
+        type: "POST",
+        data: $('#Form').serialize(),
+         beforeSend: function(){
+               $('#btn_save').prop('disabled', true);
+               $('#btn_save').html('Loading');
+          }, 
+        success: function(data) {
+
+            $('#btn_save').prop('disabled', false);
+           $('#btn_save').html('Save');
+             $("#Form").trigger("reset");
+
+             // alert("hi");
+          swal({
+            title:"",
+            text:"Data Submitted Successfully",
+            type:"success",
+            showCancelButton: false, 
+            showConfirmButton: false,
+             width: '1000px',
+            timer:1000
+        });
+           $('#Form').parsley().destroy();
+           $('#Form').parsley();
+           a=false;
+
+                }
+      });
+    	}
+      }
+ // }
